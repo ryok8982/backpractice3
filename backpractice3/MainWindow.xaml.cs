f5b399a3db1d7f83ec5a85a1e682cd6281ec7aeb
遼佑 大西 <ryo.k8982@gmail.com>
2021-08-24 22:58:19 +0900

    prot1

--- backpractice3/MainWindow.xaml.cs
+++ backpractice3/MainWindow.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Shapes;
 using System.Xml;
 using Microsoft.Win32;
 using System.Xml.Linq;
-using System.Xml.XPath;
 
 namespace backPractice
 {
@@ -29,11 +28,10 @@ namespace backPractice
 
             InitializeComponent();
         }
+        private void ShowData()
+        {
+            ListBox1.Items.Clear();
 
-
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            //string szFile = "../..//test004.xml";
-            //string szFull = System.IO.Path.GetFullPath(szFile);
             XElement xml = XElement.Load(@"test004.xml");
             var infos = xml.Elements("ListOfInspectionMethod").Select(x => x.Element("MethodName"));
             {
@@ -43,6 +41,13 @@ namespace backPractice
                     ListBox1.Items.Add(info);
                 }
             };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) 
+        {
+
+            ShowData();
+
         }
     
 
@@ -63,6 +68,33 @@ namespace backPractice
             CS_auto.IsChecked = false;
             CS_min.IsChecked = false;
         }
+
+        private void button_export_Click(object sender, RoutedEventArgs e)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "  ";
+            settings.Encoding = Encoding.UTF8;
+
+            int IntTest = Convert.ToInt32(CS_auto.IsChecked);
+            int IntMax = Convert.ToInt32(CS_max.IsChecked);
+            int IntMin = Convert.ToInt32(CS_min.IsChecked);
+
+            var xml = new XElement("ListOfInspectionMethod",
+            new XElement("MethodName", TextBox1.Text),
+            new XElement("comparisonSetting",
+            new XElement("autoAdjustment", IntTest),
+            new XElement("CS_max", IntMax),
+            new XElement("CS_min", IntMin),
+            new XElement("MaximumResolution", CS_maxResolution.Text)));
+
+
+            xml.Save(TextBox_export.Text + ".xml");
+
+            MessageBox.Show("Write Complete!");
+
+        }
+    
 
         private void button_write_Click(object sender, RoutedEventArgs e)
         {
@@ -86,32 +118,66 @@ namespace backPractice
 
             MessageBox.Show("Write Complete!");
 
+            ShowData();
         }
 
 
-            private void PrintText(object sender, RoutedEventArgs e)
-            { 
-            var xml = XElement.Load(@"test004.xml");
-            var lbi = ListBox1.SelectedItem.ToString();
-            //メンバー情報のタグ内の情報を取得する
-            var cs = (from item in xml.Elements("ListOfInspectionMethod")
-                       where item.Element("MethodName").Value == lbi
-                       select item ).Single();
+        private void PrintText(object sender, RoutedEventArgs e)
+        { 
+        var xml = XElement.Load(@"test004.xml");
+        var lbi = ListBox1.SelectedItem.ToString();
+        //メンバー情報のタグ内の情報を取得する
+        var cs = (from item in xml.Elements("ListOfInspectionMethod")
+                    where item.Element("MethodName").Value == lbi
+                    select item ).Single();
 
-            var tes = cs.Element("comparisonSetting");
-                int auto = Convert.ToInt32(tes.Element("autoAdjustment").Value);
+        var tes = cs.Element("comparisonSetting");
+            int auto = Convert.ToInt32(tes.Element("autoAdjustment").Value);
+            CS_auto.IsChecked = Convert.ToBoolean(auto);
+
+            int max = Convert.ToInt32(tes.Element("CS_max").Value);
+            CS_max.IsChecked = Convert.ToBoolean(max);
+
+            int min = Convert.ToInt32(tes.Element("CS_min").Value);
+            CS_min.IsChecked = Convert.ToBoolean(min);
+
+            string MaxRes = tes.Element("MaximumResolution").Value;
+            CS_maxResolution.Text = MaxRes;
+
+        }
+        private void button_reference_Click(object sender, RoutedEventArgs e)
+        {
+            // ダイアログのインスタンスを生成
+            var dialog = new OpenFileDialog();
+
+            // ファイルの種類を設定
+            dialog.Filter = "テキストファイル (*.xml)|*.xml|全てのファイル (*.*)|*.*";
+
+
+
+            // ダイアログを表示する
+            if (dialog.ShowDialog() == true)
+            {
+                // 選択されたファイル名 (ファイルパス) をメッセージボックスに表示
+                //xmlファイルを指定する
+                string filename = dialog.FileName;
+                var doc = XElement.Load(filename);
+
+                XElement cs = doc.Element("comparisonSetting");
+
+                int auto = Convert.ToInt32(cs.Element("autoAdjustment").Value);
                 CS_auto.IsChecked = Convert.ToBoolean(auto);
 
-                int max = Convert.ToInt32(tes.Element("CS_max").Value);
+                int max = Convert.ToInt32(cs.Element("CS_max").Value);
                 CS_max.IsChecked = Convert.ToBoolean(max);
 
-                int min = Convert.ToInt32(tes.Element("CS_min").Value);
+                int min = Convert.ToInt32(cs.Element("CS_min").Value);
                 CS_min.IsChecked = Convert.ToBoolean(min);
 
-                string MaxRes = tes.Element("MaximumResolution").Value;
+                string MaxRes = cs.Element("MaximumResolution").Value;
                 CS_maxResolution.Text = MaxRes;
-
             }
-     
+
+        }
     }
 }
